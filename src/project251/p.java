6732a022bfdec6b7f7410b112c01233886956cca
shrooq <shoroqalsadiq@gmail.com>
2024-04-08 01:43:44 +0300

    mm

---
+++ src/project251/p.java
@@ -0,0 +1,15 @@
+package project251;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SHROOQAS
+ */
+public class p {
+    
+}
