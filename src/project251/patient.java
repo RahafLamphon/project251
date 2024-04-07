3c9233dd9ca8a3c78fa4bdd9fdb55bc31bc0cec8
ShrooqAlsadiq <157583573+ShrooqAlsadiq@users.noreply.github.com>
2024-04-08 00:55:28 +0300

    Update patient.java

--- src/project251/patient.java
+++ src/project251/patient.java
@@ -5,5 +5,52 @@ public class Patient {
     String name;
     String username;
     int password;
-    //Catogery catogery;
+   // Catogery catogery;
+
+    
+    //getters
+   /* public Catogery getCatogery() {
+        return catogery;
+    }*/
+    
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    //setters
+  /*  public void setCatogery(Catogery catogery) {
+        this.catogery = catogery;
+    }*/
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+    
+    @Override
+   /* public String toString() {
+        return "Patient:\n" + "id=" + id + "\nname=" + name + "\ncatogery=" + catogery ;
+    }*/
 }
