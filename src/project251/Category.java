831bf8e672f507168765d9f7392d139a9014aab3
RahafLamphon <rlamphon@stu.kau.edu.sa>
2024-04-08 23:41:16 +0300

    rahaf

---
+++ src/project251/Category.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project251;
+
+/**
+ *
+ * @author rahaf
+ */
+public class Category {
+    String catName;
+    String catType;
+
+    public Category(String catName, String catType) {
+        this.catName = catName;
+        this.catType = catType;
+    }
+
+    public Category() {
+    }
+
+    public String getCatName() {
+        return catName;
+    }
+
+    public void setCatName(int option) {
+    if(option==1)
+        catName="obsessive compulsive Disorder";
+    else if(option==2)
+        catName="Anxiety Disorder";
+    else if(option==3)
+        catName="Schizophrenia Disorder";
+    else if(option==4)
+        catName="Bipolar Disorder"; 
+    else if(option==5)
+        catName="Major Depressive Disorder"; 
+    else if(option==6)
+        catName="Acute stress Disorder"; 
+    else if(option==7)
+        catName="Eating Disorders"; 
+    else if(option==8)
+        catName="Attention-Deficit Hyperactivity Disorder"; 
+    else if(option==9)
+        catName="Insomnia";  
+    }
+
+    public String getCatType() {
+    return catType="1.obsessive compulsive Disorder"+"/n"+
+            "2.Anxiety Disorder"+"/n"+"3.Schizophrenia Disorder"+"/n"+"4.Bipolar Disorder"+"/n"+"5.Major Depressive Disorder"+"/n"+
+            "6.Acute stress disorder"+"/n"+"Posttraumatic stress Disorder"+"/n"+"7.Eating Disorders"+"/n"+"8.Attention-Deficit Hyperactivity Disorder"+
+            "/n"+"9.Insomnia";
+        
+    }
+
+    public void setCatType(String catType) {
+        this.catType = catType;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" + "catName=" + catName + ", catType=" + catType + '}';
+    }
+   
+}
--- src/project251/Project251.java
+++ src/project251/Project251.java
@@ -5,6 +5,8 @@
  */
 package project251;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rahaf
@@ -15,11 +17,19 @@ public class Project251 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
          System.out.println("shrooq alsadiq");
          System.out.println("Jehan Meqdad");
          System.out.println("Shjoon Nasser Almutairi!");
          System.out.println("Raghad Alzahrani :)");
          System.out.println("Rahaf Lamphon");
+         Category t=new Category();
+         System.out.println("heres a list of category Diseases");
+         System.out.println(t.getCatType());
+         System.out.println("please select a number");
+         
+         
+
     }
     
 }
