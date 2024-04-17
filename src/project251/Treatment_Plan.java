ecd8578d913aa4adac593a1d9dc415823b0e50a5
ShjoonahAlmutairi <Shjoon.ns.al@gmail.com>
2024-04-17 14:08:58 +0300

    Write a simple comment

---
+++ src/project251/Treatment_Plan.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project251;
+
+/**
+ *
+ * @author 96650
+ */
+public class Treatment_Plan {
+    String TreatmentPlan_Name;
+    int treatmentPlan_ID;
+    // sessions array
+    Category categoryType;
+
+    public Treatment_Plan(String TreatmentPlan_Name, int treatmentPlan_ID, Category categoryType) {
+        this.TreatmentPlan_Name = TreatmentPlan_Name;
+        this.treatmentPlan_ID = treatmentPlan_ID;
+        this.categoryType = categoryType;
+    }
+
+    public String getTreatmentPlan_Name() {
+        return TreatmentPlan_Name;
+    }
+
+    public void setTreatmentPlan_Name(String TreatmentPlan_Name) {
+        this.TreatmentPlan_Name = TreatmentPlan_Name;
+    }
+
+    public int getTreatmentPlan_ID() {
+        return treatmentPlan_ID;
+    }
+
+    public void setTreatmentPlan_ID(int treatmentPlan_ID) {
+        this.treatmentPlan_ID = treatmentPlan_ID;
+    }
+
+    public Category getCategoryType() {
+        return categoryType;
+    }
+
+    public void setCategoryType(Category categoryType) {
+        this.categoryType = categoryType;
+    }
+    
+    
+}
