65754ec29175d60e643166431b178cce23f51ee7
Jehan Meqdad <116122834+jehanmeqdad@users.noreply.github.com>
2024-04-22 19:58:36 +0300

    Create Exercise.java

---
+++ src/project251/Exercise.java
@@ -0,0 +1,42 @@
+
+package project251;
+
+public class Exercise {
+    
+    String ContentOfEx;
+    String AnswerOfEx;
+    Boolean availability;
+
+    public Exercise(String ContentOfEx, String AnswerOfEx, Boolean availability) {
+        this.ContentOfEx = ContentOfEx;
+        this.AnswerOfEx = AnswerOfEx;
+        this.availability = availability;
+    }
+
+    public String getContentOfEx() {
+        return ContentOfEx;
+    }
+
+    public void setContentOfEx(String ContentOfEx) {
+        this.ContentOfEx = ContentOfEx;
+    }
+
+    public String getAnswerOfEx() {
+        return AnswerOfEx;
+    }
+
+    public void setAnswerOfEx(String AnswerOfEx) {
+        this.AnswerOfEx = AnswerOfEx;
+    }
+
+    public Boolean getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(Boolean availability) {
+        this.availability = availability;
+    }
+    
+    
+    
+}
